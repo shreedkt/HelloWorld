31561e7c8241485c16e6b312c49c6301277d7d4a
Deepak Kumar Tiwari <deepakkumartiwari999@gmail.com>
2022-05-28 15:10:37 +0530

    Add files via upload

---
+++ MCR101-Hello World/src/World.java
@@ -0,0 +1,10 @@
+
+public class World {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Trying to connect Github through Eclipse");
+
+	}
+
+}
